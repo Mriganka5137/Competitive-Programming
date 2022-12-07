1aba2ae1c4db19b339e04016666c28732fb65911
Mriganka Gogoi <gogoimriganka09@gmail.com>
2022-12-07 08:33:50 +0530

    String Class

--- STL QuickStart/stringInSTL.cpp
+++ STL QuickStart/stringInSTL.cpp
@@ -8,5 +8,31 @@ using namespace std;
 int main()
 {
 
-  string s0 return 0;
+  string s0 = "Hello";
+  string s1("hello World!");
+
+  string s2(s1);
+
+  string name = " Mriganka";
+
+  s0.append(name);
+  string s3 = s0;
+
+  if (s0.empty())
+  {
+    cout << "I am so empty😳" << endl;
+  }
+
+  else
+  {
+    cout << "I am not empty" << endl;
+    /* code */
+  }
+
+  cout << s0 << endl;
+  cout << s1 << endl;
+  cout << s2 << endl;
+  cout << s3 << endl;
+
+  return 0;
 }
